58e0cd7cecdceacfb0c966cfeaec6111af596d9c
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-21 09:21:57 +0530

    Add files via upload

---
+++ CreateArray-DSA.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Main{
+    public static void main(String[] args){
+        int[] intArray;
+        intArray = new int[3];
+        intArray[0]=1;
+        intArray[1]=2;
+        intArray[2]=3;
+        System.out.println(Arrays.toString(intArray));
+
+        //All together 
+
+
+        String ritesh[] = {"r" , "i", "t", "e" ,"s", "h"};
+        System.out.println(Arrays.toString(ritesh));
+
+    }
+}
