874ff238f1fb648ee7d18a5ed8bac0fd49ff458a
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-21 09:53:14 +0530

    Create AccessElements.java

---
+++ AccessElements.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Main {
+    public static void main(String[] args){
+       int[] ritesh;
+       ritesh = new int[3];
+       ritesh[0]=1;
+       ritesh[1]=2;
+
+       var a = ritesh[0];
+       System.out.println(a);
+    }
+}
